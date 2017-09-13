ef9eaeddcd2a73c3a19603219169150a7790de16
zpjgit <zpj.love@foxmail.com>
2017-09-13 11:16:53 +0800

    自定义控件 第二种方法：在主布局里引入子活动  效果一样 在方法二的基础上添加子布局响应

--- app/src/main/java/com/zpj/framelayout_test/titleActivity.java
+++ app/src/main/java/com/zpj/framelayout_test/titleActivity.java
@@ -1,21 +1,43 @@
 package com.zpj.framelayout_test;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class titleActivity extends LinearLayout {
+    @Override
+    public void setOnClickListener(@Nullable OnClickListener l) {
+        super.setOnClickListener(l);
+    }
+
     public titleActivity(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.title, this);
+
+        Button titleBack = (Button) findViewById(R.id.title_back);
+        Button titleEdit = (Button) findViewById(R.id.title_edit);
+
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ( (Activity) getContext() ).finish();
+            }
+        });
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "You clicked Edit button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
 }
